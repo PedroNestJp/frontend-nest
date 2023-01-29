7107f68fb20dfc7d263919465e7d4a6d9c7abb11
Pedro Nest <pedronestjp@gmail.com>
2023-01-29 10:18:26 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import cors from 'cors'
 
-App.use(cors)
+App.use(cors())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
