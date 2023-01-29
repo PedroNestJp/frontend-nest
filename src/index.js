0f1cb8cca45ded9b89ec755aa572e740d8f498cd
Pedro Nest <pedronestjp@gmail.com>
2023-01-29 10:12:11 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import cors from 'cors'
+
+App.use(cors)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
