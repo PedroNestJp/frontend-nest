5b7f966b3ff49a9a1d86020d519e6d4f9a14f840
Pedro Nest <pedronestjp@gmail.com>
2023-02-01 16:21:52 -0300

    rm react-dom, cors, mysql, webvitals

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from '../pages/home/Home'
+import HomeScreen from '../pages/home/Home'
 import CreateProduct from '../pages/product/CreateProduct'
 import ListProducts from '../pages/product/ListProducts'
 import EditProduct from '../pages/product/EditProduct'
@@ -8,7 +8,7 @@ import ListUser from '../pages/user/ListUser'
 import CreateUSer from '../pages/user/CreateUser'
 import UpdateUser from '../pages/user/UpdateUser'
 import Login from '../pages/Login/Login'
-import Register from '../pages/register/Register'
+import RegisterScreen from '../pages/register/Register'
 import ProductScreen from '../pages/product/ProductScreen'
 import FinalizeOrder from '../pages/finalizeOrder/FinalizeOrderScreen'
 import RegisterComponents from '../components/RegisterComponents'
@@ -16,20 +16,21 @@ import RegisterComponents from '../components/RegisterComponents'
  const Rotas = () => {
     return(
         <Routes>
-          <Route path='/' index  element={<Home />} />
-          <Route path='/home' index  element={<Home />} />
+          <Route path='/' index  element={<HomeScreen />} />
+          <Route path='/home' index  element={<HomeScreen />} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<RegisterScreen/>} />
+          <Route path='/productScreen' element={<ProductScreen/>} />
+          <Route path='/finalizeOrder' element={<FinalizeOrder/>} />
+          
           <Route path='/products' element={<ListProducts />} />
           <Route path='/product/create' element={<CreateProduct />} />
+          <Route path='/createUser' element={<CreateUSer/>} />
           <Route path='/product/update/:id' element={<EditProduct />} />
           <Route path='/product/delete/:id' element={<EditProduct />} />
           <Route path='/users' element={<ListUser/>} />
           <Route path='/user/create' element={<RegisterComponents/>} />
           <Route path='/user/update/:id' element={<UpdateUser/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
-          <Route path='/productScreen' element={<ProductScreen/>} />
-          <Route path='/finalizeOrder' element={<FinalizeOrder/>} />
-          <Route path='/createUser' element={<CreateUSer/>} />
         </Routes>
     )
 }
